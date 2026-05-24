585f08c23a790a1caf2b974b1372554a414fd28e
ritwik2510 <ritwik2510@gmail.com>
2026-05-24 18:45:01 -0500

    routes.py written

--- Module1/app/routes.py
+++ Module1/app/routes.py
@@ -2,6 +2,13 @@ from app import app
 from flask import render_template
 
 @app.route("/")
-
 def home():
-    return render_template("home.html")
+    return render_template("home.html", active="home")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", active="contact")
+
+@app.route("/projects")
+def projects():
+    return render_template("project.html", active="projects")
