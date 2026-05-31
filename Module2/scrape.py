5f6e5803709675c32d93c6be12051e6feffa69a9
ritwik2510 <ritwik2510@gmail.com>
2026-05-30 21:28:23 -0500

    worked on scrape.py

--- Module2/scrape.py
+++ Module2/scrape.py
@@ -6,3 +6,32 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
+def build_url(page):
+
+    params = {"page": page}
+
+    return "https://www.thegradcafe.com/survey?" + urlencode(params)
+
+def get_driver():
+
+    driver = webdriver.Chrome()
+    return driver
+
+
+def scrape_data():
+
+    driver = get_driver()
+
+    all_records = []
+
+    for page in range(1, 1000):
+
+        url = build_url(page)
+
+        driver.get(url)
+
+        time.sleep(2)
+
+        html = driver.page_source
+
+        soup = BeautifulSoup(html, "html.parser")
